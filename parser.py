2120a746e5e1115599b523bae2ae8cb349fe69ea
smahraz <smahraz@student.1337.ma>
2026-05-17 12:43:07 +0100

    feat(parser): extract connections from metadata file

--- parser.py
+++ parser.py
@@ -30,20 +30,59 @@ def extract_inline_metadata(
     return metadata
 
 
-class Zone:
+class Connection:
+    zones: set[str]
+    metadata: dict[str, str]
 
-    def __init__(
-        self,
-        name: str,
-        x: int,
-        y: int,
-        metadata: dict[str, str]
-    ) -> None:
+    def __init__(self, zones: tuple[str, str], **metadata: str) -> None:
+        self.zones = set(zones)
+        # convert to parse error or something
+        assert len(self.zones) == 2
+        self.metadata = metadata
+
+    @staticmethod
+    def from_metadata(
+        line: str, raise_error: Callable[[str], ParseError]
+    ) -> "Connection":
+        match_ = re.fullmatch(
+            r"connection\s*:\s*(\w+)-(\w+)(\s+(\[.*\]))?",
+            line
+        )
+        if not match_:
+            raise raise_error("broken connection definition")
+
+        inline_metadata: dict[str, str] = {}
+        if match_.group(4):
+            inline_metadata = extract_inline_metadata(
+                    match_.group(4),
+                    raise_error
+            )
+        if match_.group(1) == match_.group(2):
+            raise raise_error("zone connect to itself")
+        return Connection(
+            (match_.group(1), match_.group(2)),
+            **inline_metadata
+        )
+
+    def to_tuple(self) -> tuple[str, str]:
+        zones: list[str] = list(self.zones)
+        zones.sort()
+        return zones[0], zones[1]
+
+    def __repr__(self) -> str:
+        return f"Connection('{', '.join(self.zones)}')"
+
+
+class Zone:
+    connections: list[Connection]
+
+    def __init__(self, name: str, x: int, y: int, **metadata: str) -> None:
         self.name = name
 
         self.x = x
         self.y = y
         self.metadata = metadata
+        self.connections = []
 
     def __str__(self) -> str:
         return f"Zone({self.name}: {self.x},{self.y})"
@@ -58,7 +97,7 @@ class Zone:
             r"[a-z_]+\s*:\s*(\w+)\s+(\d+)\s+(\d+)(\s+(\[.*\]))?", line
         )
         if not match_:
-            raise raise_error("broken zone metadata")
+            raise raise_error("broken zone definition")
         if match_.group(5):
             inline_metadata = extract_inline_metadata(
                 match_.group(5),
@@ -69,8 +108,11 @@ class Zone:
             match_.group(1),
             int(match_.group(2)),
             int(match_.group(3)),
-            inline_metadata,
+            **inline_metadata,
         )
+
+    def add_connection(self, connection: Connection) -> None:
+        self.connections.append(connection)
 
 
 class DataParser:
@@ -89,6 +131,24 @@ class DataParser:
         self.number_of_drones = self._nb_drones(self.lines[0])
         self.lines = self.lines[1:]
         self.zone = self._extract_zones_conection()
+
+    def _create_connection(
+        self, line_num: int, line: str, connections: set[tuple[str, str]]
+    ) -> None:
+        connection = Connection.from_metadata(
+            line, lambda msg: self._raise_error(line_num, msg)
+        )
+
+        zones = connection.to_tuple()
+        if zones in connections:
+            raise self._raise_error(line_num, "duplicated connection")
+        if any(z not in self.zones for z in zones):
+            raise self._raise_error(
+                line_num,
+                "connecting to non existsing zone"
+            )
+        for z in zones:
+            self.zones[z].add_connection(connection)
 
     def _add_zone(
         self, line_num: int, line: str, coordinate: set[tuple[int, int]]
@@ -109,11 +169,12 @@ class DataParser:
 
     def _extract_zones_conection(self) -> dict[str, Zone]:
         coordinate: set[tuple[int, int]] = set()
+        connections: set[tuple[str, str]] = set()
         for line_num, line in self.lines:
             if re.fullmatch(r"^(start_hub|end_hub|hub)\s*:.+", line):
                 self._add_zone(line_num, line, coordinate)
             elif re.fullmatch(r"^connection\s*:.+", line):
-                pass
+                self._create_connection(line_num, line, connections)
             elif re.fullmatch(r"^nb_drones\s*:.+", line):
                 raise self._raise_error(line_num, "re-assign `nb_drones`")
             elif re.match(r"^[\w]+\s*:.+", line):
@@ -152,3 +213,6 @@ if __name__ == "__main__":
         metadata = file.read()
 
     d = DataParser(metadata, PATH)
+    for z in d.zones.values():
+        print(z)
+        print(z.connections)
