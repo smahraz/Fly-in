425bf84d9e1b5af5462130e4ef307fc8a3638dfe
smahraz <smahraz@student.1337.ma>
2026-06-14 12:42:59 +0100

    feat(visualizer): display zoom, drones & turns

--- flyin/gui/visualizer.py
+++ flyin/gui/visualizer.py
@@ -8,11 +8,7 @@ BG_COLOR = (0x66, 0x33, 0x99, 0xff)
 WIDTH = 1200
 HEIGHT = 800
 
-SIDEPANEL_BG_COLOR = (0x4c, 0x4e, 0x96, 0xff)
-SIDEPANEL_WIDTH = 250
-SIDEPANEL_HEIGHT = HEIGHT
-
-PAUSE_TIME = 1.0
+PAUSE_TIME = 0.3
 
 ZONE_RADIUS = 50.0
 
@@ -21,47 +17,34 @@ ZONE_SPACING = SCALE + 40
 TITLE = b"Fly-in"
 
 
-class SidePanel:
-    def __init__(self, parsing_data: DataParser) -> None:
+class DrawInfo:
+    def __init__(self, parsing_data: DataParser, turn: int, zoom: float) -> None:
         self.parsing_data = parsing_data
-        self.y = 0
-        self.x = WIDTH - SIDEPANEL_WIDTH
+        self.turn = turn
+        self.zoom = zoom
+        self.y = 10
+        self.x = 20
 
     def draw(self) -> None:
-        rl.DrawRectangle(
-            self.x,
-            self.y,
-            SIDEPANEL_WIDTH,
-            SIDEPANEL_HEIGHT,
-            SIDEPANEL_BG_COLOR
+        self._draw_textwidget(
+            "Drones: "
+            f"{self.parsing_data.end_hub.drone_count}"
+            f"/{self.parsing_data.number_of_drones}"
         )
-        self._draw_textwidget1(
-            "Number of Drones:",
-            f"{self.parsing_data.number_of_drones}"
-        )
+        self._draw_textwidget(f"Turn: {self.turn}")
+        self._draw_textwidget(f"Zoom: {self.zoom:.2}x")
 
-    def _draw_textwidget1(self, title: str, value: str) -> None:
-        TITLE_TEXT_SIZE = 17
-        VALUE_TEXT_SIZE = 15
-        self.y += 20
-        x = self.x + 12
+    def _draw_textwidget(self, title: str) -> None:
+        TITLE_TEXT_SIZE = 20
 
         rl.DrawText(
             title.encode(),
-            x,
+            self.x,
             self.y,
             TITLE_TEXT_SIZE,
             rl.WHITE
         )
         self.y += 20
-        x += 20
-        rl.DrawText(
-            value.encode(),
-            x,
-            self.y,
-            VALUE_TEXT_SIZE,
-            rl.WHITE
-        )
 
 
 class DrawDrone:
@@ -140,6 +123,7 @@ class Visualizer:
 
     def loop(self) -> None:
         pause_time = 0.0
+        turn = 0
         while not rl.WindowShouldClose():
             wheel = rl.GetMouseWheelMove()
             if wheel:
@@ -155,11 +139,12 @@ class Visualizer:
             if self._draw_drones():
                 if pause_time > PAUSE_TIME:
                     pause_time = 0
+                    turn += 1
                     print(" ".join(str(d) for d in next(self.simulation)))
                 else:
                     pause_time += rl.GetFrameTime()
             rl.EndMode2D()
-            self._draw_sidepanel()
+            DrawInfo(self.parsing_data, turn, self._camera.zoom).draw()
             rl.EndDrawing()
 
     def _draw_drones(self) -> bool:
@@ -168,9 +153,6 @@ class Visualizer:
         for d in self.drones:
             next_ &= DrawDrone(d, dt).draw()
         return next_
-
-    def _draw_sidepanel(self) -> None:
-        SidePanel(self.parsing_data).draw()
 
     def _draw_zones(self) -> None:
         FONT_SIZE = 18
