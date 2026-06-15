05dfef0d4e8a009a64e00d4b2c2709501bd33403
smahraz <smahraz@student.1337.ma>
2026-06-15 22:36:07 +0100

    feat: handle file errors & missing arguments

--- flyin/__main__.py
+++ flyin/__main__.py
@@ -1,11 +1,27 @@
 from flyin import Visualizer, DataParser
 from sys import argv
+from .exceptions import ParseError, MapError
 
+
+def try_read_file(file_path: str) -> str:
+    try:
+        with open(file_path, "r") as mp_file:
+            return mp_file.read()
+    except BaseException as e:
+        print(f"Error: {e}")
+        exit(1)
+
+
+if len(argv) != 2:
+    print("Usage: python main.py <map_path>")
+    exit()
 
 map_path = argv[1]
 
-
-with open(map_path, "r") as mp_file:
-    dp = DataParser(mp_file.read())
-
-Visualizer.start(dp)
+try:
+    dp = DataParser(try_read_file(map_path))
+    Visualizer.start(dp)
+except ParseError as e:
+    print(f"ParseError: {e}")
+except MapError:
+    print("Error: There is no path between start_hub and end_hub")
